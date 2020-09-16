4bd95a2eba31e06fbae5feab706824ab233e33d2
adamzahran14 <adamzahran72@gmail.com>
2020-09-16 19:10:23 +0700

    HelloWorld.java

---
+++ cobacoba2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Adam Zahran");
+		System.out.println("Kelas: D3IF4404");
+		System.out.println("NIM  : 6706202084");
+	}
+}
